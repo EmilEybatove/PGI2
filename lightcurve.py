49233b3c41baf61f8b08fd116d6b50fa3db5b2bf
Emil <emileibatov@yandex.ru>
2023-09-12 22:01:09 +0300

    в файле lightcurve.py два графика, достраивающих данные при увеличении и синхронизированные (но не совсем)

---
+++ lightcurve.py
@@ -0,0 +1,75 @@
+from h5py import File
+import plotly.graph_objects as go, trace_updater
+from plotly_resampler import FigureResampler
+import numpy as np
+from dash import Dash, html, dcc, Input, Output
+import pandas as pd
+
+app = Dash(__name__)
+
+
+def lightcurve(filename=f'./2022_23/2022_23/2022-10-05-18_32-19_32.mat'):
+    file = File(filename)
+
+    time = np.ravel(file.get("unixtime_dbl_global"))
+    time = pd.to_datetime(pd.Series(time), unit="s").to_numpy()
+    light = np.ravel(file.get("lightcurvesum_global"))
+
+    fig = FigureResampler(go.Figure())
+    fig.add_trace(go.Scattergl(name='lightcurve', showlegend=True), hf_x=time, hf_y=light)
+    return fig
+
+
+fig = lightcurve()
+fig2 = lightcurve()
+
+app.layout = html.Div([
+    dcc.Graph(id="graph-id", figure=fig),
+    dcc.Graph(id="1234567", figure=fig2),
+    html.Div(id="div"),
+    trace_updater.TraceUpdater(id="trace-updater", gdID="graph-id"),
+    trace_updater.TraceUpdater(id="b", gdID="1234567")
+])
+
+fig.register_update_graph_callback(app, "graph-id", "trace-updater")
+fig2.register_update_graph_callback(app, "1234567", "b")
+
+first = False
+second = False
+
+
+# @app.callback(Output("1234567", "relayoutData"),
+#               Output("1234567", "figure"),
+#               Input('graph-id', 'relayoutData'))
+# def display_relayout_data(relayoutData):
+#     fig2.plotly_update(relayout_data=relayoutData)
+#     return relayoutData, fig2
+
+
+@app.callback(Output("1234567", "relayoutData"),
+              Output("1234567", "figure"),
+              Input('graph-id', 'relayoutData'))
+def display_relayout_data(relayoutData):
+    global first, second
+    if not second:
+        first = True
+        fig2.plotly_update(relayout_data=relayoutData)
+        return relayoutData, fig2
+    first = False
+    second = False
+
+
+@app.callback(Output("graph-id", "relayoutData"),
+              Output("graph-id", "figure"),
+              Input('1234567', 'relayoutData'))
+def display_relayout_data(relayoutData):
+    global first, second
+    if not first:
+        second = True
+        fig.plotly_update(relayout_data=relayoutData)
+        return relayoutData, fig
+    second = False
+    first = False
+
+
+app.run('127.0.0.1', port=5000)  # debug=True
