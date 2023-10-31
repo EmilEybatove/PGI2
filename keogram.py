f74a92fab96c965d04426c0901d11961469bc915
Emil <emileibatov@yandex.ru>
2023-10-31 12:59:57 +0300

    исправил баги. Улучшил работу функции масштабирования

--- keogram.py
+++ keogram.py
@@ -15,24 +15,18 @@ app = Dash(__name__)
 class Keogram:
     def __init__(self, filename="./mat/matlab.mat", max_n_samples=1000):
         self.filename = filename
+        self.max_n_samples = max_n_samples
         file = File(filename)
         time = np.ravel(file.get("unixtime_dbl_global"))
-        time = pd.to_datetime(pd.Series(time), unit="s").to_numpy()
+        self.time = pd.to_datetime(pd.Series(time), unit="s").to_numpy()
         diag_global = file.get("diag_global")
         diag_global = np.rot90(diag_global)
         file.close()
-        self.min = diag_global.min()
-        self.max = diag_global.max()
+        self.min = diag_global.min(initial=0)
+        self.max = diag_global.max(initial=0)
         self.size = time.size
         self.yrange = [0, 15]
-        self.first_diag = {
-            "x": time,
-            "y": diag_global[0],
-            "max_n_samples": max_n_samples,
-            "downsampler": MinMaxAggregator(),
-            "gap_handler": MedDiffGapHandler()
-        }
-        self.data = diag_global[1:]
+        self.data = diag_global
         data = self.update()
         self.figure = go.Figure(data=go.Heatmap(x=data[0], z=data[1]))
         self.slider = dcc.RangeSlider(self.min,
@@ -44,8 +38,8 @@ class Keogram:
                                       tooltip={"placement": "left", "always_visible": True},
                                       id="keogram_slider")
         self.dcc = html.Div([
-            dcc.Graph(id="keogram", figure=self.figure, style={'float': 'left', 'margin': 'auto', "width": "90%"}),
-            html.Div(self.slider, style={'float': 'right', 'margin': 'auto'})
+            dcc.Graph(id="keogram", figure=self.figure, style={'float': 'left', "width": "90%"}),
+            html.Div(self.slider, style={'float': 'right'})
         ])
 
         @app.callback(
@@ -54,40 +48,55 @@ class Keogram:
         def update_minmax(value):
             self.min = value[0]
             self.max = value[1]
-            return self.figure.update_traces({"zmin": self.min, "zmax": self.max}).update_yaxes(range=self.yrange)
+            return self.figure.update_traces({"zmin": self.min, "zmax": self.max})
 
         @app.callback(Output('keogram', 'figure'),
                       Input('keogram', 'relayoutData'))
         def update_keogram(relayoutData):
             data = keogram.update(relayoutData)
             self.yrange = data[2]
-            return go.Figure(data=go.Heatmap(x=data[0], z=data[1], zmin=self.min, zmax=self.max),
-                             layout_yaxis_range=data[2])
+            self.figure = go.Figure(data=go.Heatmap(x=data[0], z=data[1], zmin=self.min, zmax=self.max),
+                                    layout_yaxis_range=data[2])
+            return self.figure
 
     def update(self, relayoutData=None):
 
         res = []
         start, end = 0, self.size
         min_y, max_y = self.yrange[0], self.yrange[1]
+        first = {
+            "x": self.time,
+            "y": self.data[0],
+            "max_n_samples": self.max_n_samples,
+            "downsampler": MinMaxAggregator(),
+            "gap_handler": MedDiffGapHandler()
+        }
 
         if relayoutData:
             if "xaxis.range[0]" in relayoutData:
-                start, end = PlotlyAggregatorParser.get_start_end_indices(self.first_diag,
+                start, end = PlotlyAggregatorParser.get_start_end_indices(first,
                                                                           start=relayoutData["xaxis.range[0]"],
                                                                           end=relayoutData["xaxis.range[1]"],
                                                                           axis_type="date")
             if 'yaxis.range[0]' in relayoutData:
                 min_y = max(min_y, floor(relayoutData["yaxis.range[0]"]))
-            if 'yaxis.range[1]' in relayoutData:
                 max_y = min(max_y, ceil(relayoutData["yaxis.range[1]"]))
+
             if "autosize" in relayoutData or "yaxis.autorange" in relayoutData:
                 min_y, max_y = 0, 15
+        x = None
 
-        x, y, indexes = PlotlyAggregatorParser.aggregate(self.first_diag, start, end)
-        res.append(y)
+        for i in range(16):
+            diag = {
+                "x": self.time,
+                "y": self.data[i],
+                "max_n_samples": self.max_n_samples,
+                "downsampler": MinMaxAggregator(),
+                "gap_handler": MedDiffGapHandler()
+            }
+            x, y, indexes = PlotlyAggregatorParser.aggregate(diag, start, end)
+            res.append(y)
 
-        for i in range(15):
-            res.append(self.data[i][indexes])
         return x, res, [min_y, max_y]
 
 
@@ -97,6 +106,6 @@ app.layout = html.Div([
     keogram.dcc
 ])
 
-app.run(host='127.0.0.1', port=5050, debug=True)
+# app.run(host='127.0.0.1', port=5050, debug=True)
 
-# app.run(host=sys.argv[1], port=int(sys.argv[2]))
+app.run(host=sys.argv[1], port=int(sys.argv[2]))
